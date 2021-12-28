0378be731c3874d8b7a09d0d9485c6d2b4d47ec0
Abdallahsaid <34180006+Abdallahsaid@users.noreply.github.com>
2021-12-28 05:06:34 +0200

    Add files via upload

--- Stack_Task/App.c
+++ Stack_Task/App.c
@@ -17,11 +17,12 @@ int main(void)
 {
 
     u8_t u8_t_str[20];
-    while(1){
-    printf("PLEASE ENTER THE EXPRESSION\n");
-    gets(u8_t_str);
+    while (1)
+    {
+        printf("PLEASE ENTER THE EXPRESSION\n");
+        gets(u8_t_str);
 
-    printf("%s\n", balancedParentheses(u8_t_str));
+        printf("%s\n", balancedParentheses(u8_t_str));
     }
 
     return 0;
@@ -32,6 +33,7 @@ u8_t *balancedParentheses(u8_t *expression)
     u8_t u8_t_counter = 0;
     u8_t u8_t_temp_data = 0;
 
+    u8_t u8_t_last_open = 0; // this holds the value of last open parentheses and not closed yet
     s8_t s8_t_counter_curly = 0;
     s8_t s8_t_counter_round = 0;
     s8_t s8_t_counter_square = 0;
@@ -51,21 +53,53 @@ u8_t *balancedParentheses(u8_t *expression)
         {
         case ')':
             s8_t_counter_round++;
+            u8_t_last_open = ')';
             break;
         case '(':
-            s8_t_counter_round--;
+
+            if ((u8_t_last_open == '}') && (s8_t_counter_curly > 0))
+            {
+                return "UNBALANCED";
+            }
+            else if ((u8_t_last_open == ']') && (s8_t_counter_square > 0))
+            {
+                return "UNBALANCED";
+            }
+            else
+                s8_t_counter_round--;
+
             break;
         case '}':
             s8_t_counter_curly++;
+            u8_t_last_open = '}';
             break;
         case '{':
-            s8_t_counter_curly--;
+            if ((u8_t_last_open == ')') && (s8_t_counter_round > 0))
+            {
+                return "UNBALANCED";
+            }
+            else if ((u8_t_last_open == ']') && (s8_t_counter_square > 0))
+            {
+                return "UNBALANCED";
+            }
+            else
+                s8_t_counter_curly--;
             break;
         case ']':
             s8_t_counter_square++;
+            u8_t_last_open = ']';
             break;
         case '[':
-            s8_t_counter_square--;
+            if ((u8_t_last_open == '}') && (s8_t_counter_curly > 0))
+            {
+                return "UNBALANCED";
+            }
+            else if ((u8_t_last_open == ')') && (s8_t_counter_round > 0))
+            {
+                return "UNBALANCED";
+            }
+            else
+                s8_t_counter_square--;
             break;
         default:
             break;
