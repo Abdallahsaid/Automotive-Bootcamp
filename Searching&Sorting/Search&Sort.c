9851587a49607570a404f5242ca2b68f079bcdf6
Abdallahsaid <34180006+Abdallahsaid@users.noreply.github.com>
2021-12-28 23:55:48 +0200

    Add files via upload

--- Searching&Sorting/Search&Sort.c
+++ Searching&Sorting/Search&Sort.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include "STD_TYPES.h"
 
+#define  ARRAY_SIZE     5
+#define  SEARCH_FAIL   255
+
 u8_t isSorted(u32_t *arr, u8_t size);
 void bubbleSort(u32_t *arr, u8_t size);
 u8_t binarySearch(u32_t *arr, u8_t size, u32_t number);
 
 int main(void)
 {
-    u32_t arr[5] = {61, 126, 15, 26, 21}; // after sorting {15, 21, 26, 61, 126 }
+    u32_t arr[ARRAY_SIZE] = {61, 126, 15, 26, 21}; // after sorting {15, 21, 26, 61, 126 }
     u8_t u8_result=0;
    
     
-    u8_result = binarySearch(arr, 5 , 61);
+    u8_result = binarySearch(arr, ARRAY_SIZE ,61);
     
-    if ( u8_result == 255)
+    if ( u8_result == SEARCH_FAIL)
     {
         printf("Not found");
     }
@@ -74,9 +77,9 @@ u8_t binarySearch(u32_t *arr, u8_t size, u32_t number)
 
     u8_t u8_start = 0 , u8_end = size -1 , u8_mid = 0;
 
-     if (isSorted(arr, 5)==0)
+     if (isSorted(arr, size)==0)
     {
-         bubbleSort(arr, 5);
+         bubbleSort(arr, size);
     }
     else{
         //do nothing
@@ -90,21 +93,22 @@ u8_t binarySearch(u32_t *arr, u8_t size, u32_t number)
            return u8_mid;
        }
        else if (arr[u8_mid] > number)
-       {
+       {   if(u8_mid!= u8_start)
            u8_end  = u8_mid - 1;
+       else  return SEARCH_FAIL;
        }
        else if (arr[u8_mid] < number)
        {
           u8_start = u8_mid + 1;
        }
-       else
-       {
-           //do nothing
+
+       else{
+    	   //do nothing
        }
        
        
     }
     
 
-    return 255;
+    return SEARCH_FAIL;
 }
