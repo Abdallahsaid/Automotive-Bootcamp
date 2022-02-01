83e166cca165727fdf30a807e409140dae2629e0
Abdallahsaid <34180006+Abdallahsaid@users.noreply.github.com>
2022-02-01 20:48:45 +0200

    Delete UART_config.h

--- SPI_UART_APP_TASK/02-MCAL/UART/UART_config.h
+++
@@ -1,9 +0,0 @@
-/**********************************/
-/* Author   : Abdalla Abdelhafiz  */
-/* Date     : 24/1/2022            */
-/* Version  : V01                 */
-/**********************************/
-#ifndef UART_CONGIF_H_
-#define UART_CONGIF_H_
-
-#endif
